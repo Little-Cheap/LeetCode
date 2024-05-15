5391a0309ae8cca67662bab61fac6cfb7bff9136
故笙 <2969300510@qq.com>
2024-05-15 20:14:13 +0800

    字母异位词分组

---
+++ src/哈希/字母异位词分组/Solution.java
@@ -0,0 +1,61 @@
+package 哈希.字母异位词分组;
+
+import java.util.*;
+
+class Solution {
+
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> lists = new Solution().groupAnagrams1(strs);
+        for (List<String> list : lists) {
+            System.out.println(list.toString());
+        }
+
+    }
+    /**
+     * 排序
+     * @param strs
+     * @return
+     */
+    public List<List<String>> groupAnagrams1(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sorted = new String(charArray);
+            List<String> list = map.getOrDefault(sorted, new ArrayList<>());
+            list.add(str);
+            map.put(sorted, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    /**
+     * 计数
+     * @param strs
+     * @return
+     */
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            int[] counts = new int[26];
+            int length = str.length();
+            for (int i = 0; i < length; i++) {
+                counts[str.charAt(i) - 'a']++;
+            }
+            // 将每个出现次数大于 0 的字母和出现次数按顺序拼接成字符串，作为哈希表的键
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < 26; i++) {
+                if (counts[i] != 0) {
+                    sb.append((char) ('a' + i));
+                    sb.append(counts[i]);
+                }
+            }
+            String key = sb.toString();
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
